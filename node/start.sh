833ab0dd8ca04998dbffd0b58bbd6ad532811147
Ali92hm <ali92hm@gmail.com>
2017-05-18 17:57:12 -0700

    works

--- node/start.sh
+++ node/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo $GENESIS > /root/.ethereum/genesis.json
-geth init /root/.ethereum/genesis.json
+echo $GENESIS > genesis.json
+geth init genesis.json
 
 DEFAULT_ARGS="--networkid $NETWORK_ID --bootnodes enode://$(bootnode -nodekeyhex $BOOTNODE_KEY -writeaddress)@$BOOTNODE_SERVICE_HOST:$BOOTNODE_SERVICE_PORT"
 
