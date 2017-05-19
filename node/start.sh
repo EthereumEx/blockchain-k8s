4ed942e71509c5e00fb5f1baf5a6a2c8c25deced
Ali92hm <ali92hm@gmail.com>
2017-05-18 17:05:57 -0700

    refactor

--- node/start.sh
+++ node/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+echo $GENESIS > /root/.ethereum/genesis.json
 geth init /root/.ethereum/genesis.json
 
 DEFAULT_ARGS="--networkid $NETWORK_ID --bootnodes enode://$(bootnode -nodekeyhex $BOOTNODE_KEY -writeaddress)@$BOOTNODE_SERVICE_HOST:$BOOTNODE_SERVICE_PORT"
